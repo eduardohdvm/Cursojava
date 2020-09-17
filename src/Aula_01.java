bc4accb7fcc02ca5da629aa02150720ca95eff8c
Eduardo Malheiro <eduardohdvm@hotmail.com>
2020-09-17 16:53:44 -0300

    exercicios

---
+++ src/Aula_01.java
@@ -0,0 +1,10 @@
+
+public class Aula_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("olá mundo!");
+	}
+
+}
---
+++ src/course/Aula_58.java
@@ -0,0 +1,17 @@
+package course;
+
+
+public class Aula_58 {
+
+	public static void main(String[] args) {
+
+		String s = "potato apple lemon orange";
+		
+		String[] vect = s.split(" ");
+		
+		System.out.println(vect[0]);
+		System.out.println(vect[1]);
+		System.out.println(vect[2]);
+	}
+
+}
